0179e3e7234d31db4e4f08b60c7f072c0372817b
kuldeepkhatana <kuldeep.khatana3@gmail.com>
2020-03-24 13:22:32 +0530

    now working in last directory

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from .views import *
 urlpatterns = [
     path('add-cart/',AddCart.as_view()),
     path('add-cart/<int:pk>/',CartDetail.as_view()),
-    path('add-order/', AddCartApiView.as_view()),
-    # path('add-order/<int:pk>/',AddOrderProductDetail.as_view()),
+    path('add-order/', AddOrderProductDetail.as_view()),
+    path('add-order/<int:pk>/',AddOrderProductDetail.as_view()),
 
 
 ]
--- products/views.py
+++ products/views.py
@@ -26,50 +26,15 @@ class CartDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CartSerializer
 
 
-# class AddOrderProduct(generics.CreateAPIView):
-#     queryset = OrderProduct.objects.all()
-#     serializer_class = OrderProductSerializer
+class AddOrderProduct(generics.CreateAPIView):
+    queryset = OrderProduct.objects.all()
+    serializer_class = OrderProductSerializer
 
 
-# class AddOrderProductDetail(generics.CreateAPIView):
-#     queryset = OrderProduct.objects.all()
-#     serializer_class = OrderProductSerializer
+class AddOrderProductDetail(generics.CreateAPIView):
+    queryset = OrderProduct.objects.all()
+    serializer_class = OrderProductSerializer
 
 
-class AddCartApiView(APIView):
-    """API to save User Log Activity.
 
-    """
-    # permission_classes = (AllowAny,)
-    # rendered_classes = (JSONRenderer,)
-
-
-    def post(request):
-        data = request.data
-
-        try:
-            if 'cart' in data and data['cart'] != '':
-                cart = data['cart']
-            else:
-                return Response('Product missing', "", status=403)
-
-            # if 'order_place' in data and data['order_place'] == True:
-            #     if 'cart' in data and data['cart'] != '':
-            #         cart = data['cart']
-            #     else:
-            #         return Response(response.parsejson('cart is missing', "", status=403))
-
-                
-            activity_data = OrderProduct.objects.filter(cart=cart).first()
-
-            if activity_data is None:
-                temp_data = OrderProduct()
-                temp_data.order_place = order_place
-                temp_data.cart_id = cart.object.first()
-                temp_data.save()
-            
-            return Response("Success", status=201)
-
-        except Exception as exp:
-            return Response(exp, status=403)
 
